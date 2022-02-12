cb96ace47f7d9393282a8eb8622b22782fe48eb2
Deepakmaripi <maripideepak16@gmail.com>
2022-02-12 22:21:54 +0530

    Submitting My Assignment -2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -7,9 +7,15 @@ function App(props) {
  const {employee}=props;
     
 return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
+  <div className="app">
+      <img className="app-image" alt="image" src={employee.profileImg} width="100" height="100"/>
+      <h1 style={{color:'blue'}}>{employee.name}</h1>
+      <div className="info">
+        <p className='p_info'>location<br /><strong>{employee.location}</strong></p>
+        <p className='p_info'>bloodGroup<br /><strong>{employee.bloodGroup}</strong></p>
+        <p className='p_info'>age<br /><strong>{employee.age}</strong></p>
+      </div>
+    </div>
 )
 }
 
